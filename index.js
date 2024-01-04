532436700cdc41f1cdae7ccd5bb28729caed4835
Dinesh107 <dineshjr107@gmail.com>
2024-01-04 20:18:15 +0530

    3rd commit

---
+++ index.js
@@ -0,0 +1,145 @@
+/*console.log('Welcome To JS');
+console.log('Welcome To C++');
+console.log('Welcome To C');
+console.log('Welcome To JS, My Fav Lang');
+
+
+window.alert("Welcome To JS")
+
+
+Commenting in JS
+
+This code is for refrence purposes
+
+
+
+
+
+
+Variables --> Variable is container for storing data. 
+
+a variable behaves as if it was the value that it contains.
+
+Two Steps 
+
+1. declaration  (var, let, const)
+
+let a; 
+
+2. assignment (= assignment)
+
+a = 12;
+
+let a = 12 --> declaration +  assignment
+
+let age = 10;
+
+Datatypes
+
+Numbers
+
+String 
+
+Boolean 
+
+
+
+let firstName = "Java Student";
+
+let age = 21;
+
+let student = true;
+
+age = age + 1;
+
+
+console.log("Hello", firstName);
+console.log("you are", age, "years old");
+console.log("Enrolled: ", student);
+
+
+
+document.getElementById("p1").innerHTML = "Hello Students " + firstName; 
+
+document.getElementById("p2").innerHTML = "Hello Students, " + "your age, " + age;
+
+document.getElementById("p3").innerHTML = "Hello Students, " + "your are enrolled:-  " + student;
+
+
+
+
+let students = 20;
+
+students = students + 2;
+students = students - 2;
+students = students * 2;
+students = students / 2;
+
+students += 2 --> augmented assignment operator
+
+students -= 2;
+students *= 2;
+students /= 2;
+
+console.log(students);
+
+*/
+
+
+// How to accept user input
+
+// Window prompt alert
+
+// let username = window.prompt("What is your name");
+
+// console.log(username);
+
+// let username;
+
+// document.getElementById("myButton").onclick = 
+
+//  function() {
+
+//     username = document.getElementById("myText").value;
+
+//     console.log(username)
+
+//     document.getElementById("myLabel").innerHTML = "Hello " + username;
+
+// }
+
+// let age = window.prompt("How old are you!");
+
+// console.log(typeof age);
+
+// age = Number(age);
+
+// console.log(typeof age);
+
+// age += 3;
+
+
+
+// console.log("Happy birthday", age, "years old");
+
+// let x; 
+// let y;
+// let z;
+
+
+// x = Number("food");
+// y = String(3.14);
+// z = Boolean("briyani");
+
+// console.log(x, typeof x);
+
+// console.log(y, typeof y);
+
+// console.log(z, typeof z);
+
+
+
+
+
+
+
